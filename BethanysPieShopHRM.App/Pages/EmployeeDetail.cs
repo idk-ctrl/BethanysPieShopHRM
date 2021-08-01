5191c04669c1b26f989a482ff525b5df5a960961
Yevhen K <yevhenk@varyence.com>
2021-08-01 16:31:47 +0300

    Add project files.

---
+++ BethanysPieShopHRM.App/Pages/EmployeeDetail.cs
@@ -0,0 +1,32 @@
+﻿using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.ComponentsLibrary.Map;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeDetail
+    {
+		[Parameter]
+		public string EmployeeId { get; set; }
+
+		public Employee Employee { get; set; } = new Employee();
+
+		public List<Marker> MapMarkers { get; set; } = new List<Marker>();
+
+		[Inject]
+		public IEmployeeDataService EmployeeDataService { get; set; }
+		protected  override async Task OnInitializedAsync()
+		{
+			Employee = await EmployeeDataService.GetAllEmployeeDetails(int.Parse(EmployeeId));
+			MapMarkers = new List<Marker>
+			{
+				new Marker{Description = $"{Employee.FirstName} {Employee.LastName}",  ShowPopup = false, X = Employee.Longitude, Y = Employee.Latitude}
+			};
+		}
+	}
+}
